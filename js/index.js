ff430538c2656ded445bc4c634e427997585c3ca
Marco Ayquipa <rfmarco.313@gmail.com>
2023-06-18 13:04:37 -0300

    primer pre-entrega

--- js/index.js
+++ js/index.js
@@ -1,24 +1,52 @@
-console.log('hola');
 //algoritmo con condicional 
 let nombre = prompt('Ingrese su nombre')
 let apellido = prompt('Ingrese su apellido')
 let edad = prompt('Ingrese su edad en numeros')
-    if ((nombre !== '') && (apellido !== '') && (edad !== '') ){
-    console.log('Nombre, Apellido y Edad: ' + nombre + ' ' + apellido + ', ' + edad + ' años')
-    }else{
+if ((nombre !== '') && (apellido !== '') && (edad !== '') ){
+console.log('Nombre, Apellido y Edad: ' + nombre + ' ' + apellido + ', ' + edad + ' años')
+}else{
     console.log('Error: Datos no validos')
 }
 
 //algoritmo utilizando un ciclo do-while
-let cuadras = 0
-do{
-    cuadras = prompt('Ingrese numero de cuadras: ')
-    console.log(cuadras)
-}while(cuadras <= 5)
+let cuadras = parseInt(prompt('Ingrese numero: '))
+for (let i = 1; i <= 6; i++){
+    let resultado = cuadras + i
+    alert(cuadras + '+' + i + '=' + resultado )
+}
 
-//simulador interactivo - cantidad total de productos
-let producto1 = parseInt(prompt('ingresar cantidad de producto 1: '))
-let producto2 = parseInt(prompt('ingresar cantidad de producto 2: '))
-let producto3 = parseInt(prompt('ingresar cantidad de producto 3: '))
-let resultado = producto1 + producto2 + producto3
-alert('La cantidad total de productos es: ' + resultado)
+
+//simulador interactivo - carrito de compra
+alert('Seleccione el producto deseado, ingrese 0 para salir')
+let elegirProductos = Number(prompt('1-manzana $800 el kg 2-frutilla $1200 el kg 3-naranja $300 el kg'))
+let elegirCantidad;
+let total = 0
+
+const cantidad = (cant, precio) =>{
+    return cant * precio
+}
+
+while(elegirProductos !== 0){
+    switch (elegirProductos) {
+        case 1:
+            elegirCantidad = Number(prompt('El producto seleccionado es manzana,  indique la cantidad de kg'))
+            total += cantidad(elegirCantidad, 800)
+            break;
+            
+        case 2:
+            elegirCantidad = Number(prompt('El producto seleccionado es frutilla,  indique la cantidad de kg'))
+            total += cantidad(elegirCantidad, 1200)
+            break;
+
+        case 3:
+            elegirCantidad = Number(prompt('El producto seleccionado es naranja,  indique la cantidad de kg'))
+            total += cantidad(elegirCantidad, 300)
+        break;
+
+        default:
+            alert('opcion incorrecta')
+            break;
+    }
+    elegirProductos = Number(prompt('1-manzana $800 el kg 2-frutilla $1200 el kg 3-naranja $300 el kg'))
+}
+alert('El total de la compra es: ' + total)
